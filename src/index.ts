3fc75469d1b90882927505685bee16dd101644fa
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2023-02-02 16:36:28 -0300

    style: add linting

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import { initComponents } from "./components"
 import { main } from "./service"
 
 // This file is the program entry point, it only calls the Lifecycle function
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 Lifecycle.run({ main, initComponents })
